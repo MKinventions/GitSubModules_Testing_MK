96c6aff9c89d8d3df3dbc250cba74128e6f7f43f
Madhan Chiruguri <mchiruguri@semtech.com>
2025-02-11 17:02:33 +0530

    commit 2, updated with led and button feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>  // Include from submodule
+#include "led.h"
+
+LED led(13);
 
 void setup() {
     Serial.begin(9600);
-    StaticJsonDocument<200> doc;
+    JsonDocument doc;
     doc["message"] = "Hello, Git Submodule!";
     serializeJson(doc, Serial);
 }
 
 void loop() {
+
+    led.blink(500);
 }
